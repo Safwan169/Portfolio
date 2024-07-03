96deffb80975aa0f3f7340f279bae8e0f97f28f3
safwan 169 <safwanhossainmridul@gmail.com>
2024-07-03 16:30:09 +0600

    navbar update

--- src/Componants/Nabvar.jsx
+++ src/Componants/Nabvar.jsx
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiAlignJustify, FiX } from 'react-icons/fi';
 import { NavLink } from 'react-router-dom';
 // import { NavLink } from 'react-router-dom';
 
 const Nabvar = () => {
+    const [toggleNav,setToggle]=useState(false)
+    const [navBtn,setNavbtn]=useState(true)
+    const handleNav=()=>{
+        setToggle(true)
+        setNavbtn(false)
+
+    }
+
+    const handleCross=()=>{
+        setToggle(false)
+        setNavbtn(true)
+
+    }
 
 
     const navtext = <>
 
         <li>
             <NavLink to={'/'} className={({isActive}) =>
-                isActive ? '   rounded-xl  text-blue-500  h-5   hover:text-blue-500     ' : "block py-2  text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent "} >
+                isActive ? '   rounded-xl  text-blue-500     hover:text-blue-500    my-2 lg:my-0 md:my-0 ' : "block my-2 lg:my-0 md:my-0   text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent "} >
                 Home</NavLink>
         </li>
         <li>
             <NavLink to={'/about'} className={({isActive}) =>
-                isActive ? '   rounded-xl  text-blue-500  h-5   hover:text-blue-500     ' : "block py-2  text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent "} >
+                isActive ? '   rounded-xl  text-blue-500     hover:text-blue-500    my-2 lg:my-0 md:my-0 ' : "block my-2 lg:my-0 md:my-0  text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent "} >
                 About</NavLink>        </li>
         <li>
             <NavLink to={'/services'} className={({isActive}) =>
-                isActive ? '   rounded-xl  text-blue-500  h-5   hover:text-blue-500     ' : "block py-2  text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent "} >
+                isActive ? '   rounded-xl  text-blue-500     hover:text-blue-500    my-2 lg:my-0 md:my-0 ' : "block my-2 lg:my-0 md:my-0  text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent "} >
                 Services</NavLink>        </li>
         <li>
             <NavLink to={'/portfolio'} className={({isActive}) =>
-                isActive ? '   rounded-xl  text-blue-500  h-5   hover:text-blue-500     ' : "block py-2  text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent "} >
+                isActive ? '   rounded-xl  text-blue-500     hover:text-blue-500    my-2 lg:my-0 md:my-0 ' : "block my-2 lg:my-0 md:my-0  text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent "} >
                 Portfolio</NavLink>        </li>
         <li>
             <NavLink to={'/contract'} className={({isActive}) =>
-                isActive ? '   rounded-xl  text-blue-500  h-5   hover:text-blue-500     ' : "block py-2  text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent "} >
+                isActive ? '   rounded-xl  text-blue-500     hover:text-blue-500    my-2 lg:my-0 md:my-0 ' : "block my-2 lg:my-0 md:my-0  text-white  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-500 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent "} >
                 Contract</NavLink>     
                    </li>
 
@@ -40,12 +54,31 @@ const Nabvar = () => {
                     <a class="flex items-center space-x-3 rtl:space-x-reverse">
                         <span class="self-center text-2xl font-semibold whitespace-nowrap text-white ">Portfolio</span>
                     </a>
-                    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    {/* <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <svg class="w-5 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
                         </svg>
+                    </button> */}
+
+                    <button>
+                    {navBtn?<FiAlignJustify onClick={handleNav} className='md:hidden lg:hidden block text-white' size={25} />:''}
+                    {
+                                toggleNav?<div className=''>
+                                    <button>
+                                    </button>
+                                    <ul className='flex-col font-bold bg-blue-900 bg-opacity-80 w-[100px] absolute right-0 top-5'>
+                                    <FiX onClick={handleCross} size={25} className=' relative left-[60%] text-white mb-3'  />
+
+                                        {
+                                            navtext
+                                        }
+                                    </ul>
+                                </div>: <div></div>
+                            }
                     </button>
+                           
+
                     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                         <ul class="font-medium flex flex-col p-4 md:p-0 mt-4  rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             {
